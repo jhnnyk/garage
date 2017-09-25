78a4c9842ca3325d06f161bd07e6a830fd6472b1
John Knotts <jhnnyk@gmail.com>
2017-09-25 16:34:18 -0600

    add more realistic (but still mock) data

--- public/app.js
+++ public/app.js
@@ -2,21 +2,49 @@ const MOCK_FILLUPS = {
   'fillups': [
     {
       'id': '12344556',
+      'date': '3434354235',
+      'location': 'Evergreen, CO',
+      'brand': 'Loaf \'n Jug',
+      'mileage': 23562,
+      'octane': '91',
+      'gallons': 17.6,
+      'cost': 65.49,
       'text': 'this is a test fillup #1',
       'car_id': '3924508'
     },
     {
       'id': '12344556',
+      'date': '3434354235',
+      'location': 'Evergreen, CO',
+      'brand': 'Loaf \'n Jug',
+      'mileage': 23562,
+      'octane': '91',
+      'gallons': 18.8,
+      'cost': 50.49,
       'text': 'this is a test fillup #2',
       'car_id': '3924508'
     },
     {
       'id': '12344556',
+      'date': '3434354235',
+      'location': 'Evergreen, CO',
+      'brand': 'Loaf \'n Jug',
+      'mileage': 23562,
+      'octane': '91',
+      'gallons': 12.2,
+      'cost': 55.21,
       'text': 'this is a test fillup #3',
       'car_id': '3924508'
     },
     {
       'id': '12344556',
+      'date': '3434354235',
+      'location': 'Evergreen, CO',
+      'brand': 'Loaf \'n Jug',
+      'mileage': 23562,
+      'octane': '91',
+      'gallons': 16.8,
+      'cost': 35.49,
       'text': 'this is a test fillup #4',
       'car_id': '3924508'
     }
@@ -29,8 +57,13 @@ function getRecentFillups (callbackFn) {
 
 function displayFillups (data) {
   for (index in data.fillups) {
+    const fillup = data.fillups[index]
     $('#fillups').append(`
-      <li>${data.fillups[index].text}</li>
+      <li>
+        ${fillup.date}<br>
+        ${fillup.mileage} miles: ${fillup.gallons}gal $${fillup.cost}<br> 
+        ${data.fillups[index].text}
+      </li>
     `)
   }
 }
