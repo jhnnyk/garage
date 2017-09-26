ead4884c71d4371836eb4f6a3ae24ab2468e7c46
John Knotts <jhnnyk@gmail.com>
2017-09-26 10:17:40 -0600

    add Fillup model

---
+++ models.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const fillupSchema = mongoose.Schema({
+  mileage: {type: Number, required: true},
+  brand: String,
+  location: String,
+  gallons: {type: Number, required: true},
+  cost: {type: Number, required: true}
+})
+
+fillupSchema.virtual('pricePerGallon').get(function () {
+  return cost / gallons
+})
+
+const Fillup = mongoose.model('Fillup', fillupSchema)
+
+module.exports = {Fillup}
