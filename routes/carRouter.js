102ba620ddb93133c0a98bee8d9f15a2bc324a6a
John Knotts <jhnnyk@gmail.com>
2017-10-02 11:56:05 -0600

    add POST endpoint for adding a car

--- routes/carRouter.js
+++ routes/carRouter.js
@@ -25,4 +25,27 @@ router.get('/', (req, res) => {
     })
 })
 
+router.post('/', (req, res) => {
+  // check that name has been filled in
+  if (!('name' in req.body)) {
+    console.error('Missing `name` in request body')
+    return res.status(400).send(message)
+  }
+
+  // create the Car
+  Car
+    .create({
+      year: req.body.year,
+      make: req.body.make,
+      model: req.body.model,
+      name: req.body.name,
+      notes: req.body.notes
+    })
+    .then(car => res.status(201).json(car.apiRepr()))
+    .catch(err => {
+      console.error(err)
+      res.status(500).json({error: 'Something went wrong'})
+    })
+})
+
 module.exports = router
